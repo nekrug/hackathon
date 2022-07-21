496072b02d0af4066a4c58fd851cb53f6dd0f633
Nick Krug <n.e.krug@gmail.com>
2022-07-20 20:32:04 -0400

    Extremely barebones starting point

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    "id": "sampleContextMenu",
+    "title": "Sample Context Menu",
+    "contexts": ["selection"]
+  });
+});
